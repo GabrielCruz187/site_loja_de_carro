01f4219fc00973b367871ba9281e61661ea72e01
Gabriel Cruz <gabicraque10@outlook.com>
2025-03-11 13:22:17 -0300

    .env

--- back-end/src/config/db.js
+++ back-end/src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect('mongodb://localhost:27017/catalogo-carros');
+        const conn = await mongoose.connect('mongodb+srv://gabicruz102002:lojacarros123@cluster0.4ssnv.mongodb.net/catalago_carros?retryWrites=true&w=majority&appName=Cluster0');
         console.log(`MongoDB conectado: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Erro ao conectar no MongoDB: ${error.message}`);
